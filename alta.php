4cf38ad3703ba677b6aa217b9574b6ea3919d210
leonardo <lchipres@ucol.mx>
2019-10-22 17:56:01 +0000

    Agregado campo servicios funcional #6

--- alta.php
+++ alta.php
@@ -9,17 +9,18 @@ if (isset($_GET["nom"])) {
     $telefono = $_GET["tel"];
     $email = $_GET["mail"];
     $ip = $_GET["ip"];
+    $servicios = $_GET["servicios"];
 
     
     
     
     // Hay campos en blanco
-    if($name==NULL|$responsable==NULL|$telefono==NULL|$email==NULL|$ip==NULL) {
+    if($name==NULL|$responsable==NULL|$telefono==NULL|$email==NULL|$ip==NULL|$servicios==NULL) {
         echo "un campo est&aacute; vacio.";
         formRegistro();
     }else{
 
-                $query = 'INSERT INTO SALA_REMOTA (nombre,responsable, telefono, email_responsable, ip) VALUES ("'.$name.'","'.$responsable.'","'.$telefono.'","'.$email.'","'.$ip.'")';
+                $query = 'INSERT INTO SALA_REMOTA (nombre,responsable, telefono, email_responsable, ip, servicios) VALUES ("'.$name.'","'.$responsable.'","'.$telefono.'","'.$email.'","'.$ip.'","'.$servicios.'")';
                 mysqli_query($conexion,$query) or die("Problemas al insertar".mysqli_error($conexion));
                 echo 'La sala '.$name.' ha sido registrada de manera satisfactoria.<br/>';
    header('Location: index.php');
