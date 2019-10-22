5951e2b3da63a14c95c5f030f8393df8ac1ca93e
leonardo <lchipres@ucol.mx>
2019-10-22 17:45:48 +0000

    quitado campo isdn funcional #5

--- alta.php
+++ alta.php
@@ -9,17 +9,17 @@ if (isset($_GET["nom"])) {
     $telefono = $_GET["tel"];
     $email = $_GET["mail"];
     $ip = $_GET["ip"];
-    $isdn = $_GET["isdn"];
+
     
     
     
     // Hay campos en blanco
-    if($name==NULL|$responsable==NULL|$telefono==NULL|$email==NULL|$ip==NULL|$isdn==NULL) {
+    if($name==NULL|$responsable==NULL|$telefono==NULL|$email==NULL|$ip==NULL) {
         echo "un campo est&aacute; vacio.";
         formRegistro();
     }else{
 
-                $query = 'INSERT INTO SALA_REMOTA (nombre, telefono, email_responsable, ip, isdn) VALUES ("'.$name.'","'.$telefono.'","'.$email.'","'.$ip.'","'.$isdn.'")';
+                $query = 'INSERT INTO SALA_REMOTA (nombre,responsable, telefono, email_responsable, ip) VALUES ("'.$name.'","'.$responsable.'","'.$telefono.'","'.$email.'","'.$ip.'")';
                 mysqli_query($conexion,$query) or die("Problemas al insertar".mysqli_error($conexion));
                 echo 'La sala '.$name.' ha sido registrada de manera satisfactoria.<br/>';
    header('Location: index.php');
